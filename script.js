99a0e502124189e1a829d1fb12a9dfc6d2fd7c17
FathurIrwan07 <muhamadfathurirwan@gmail.com>
2024-04-01 04:33:18 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function showAlert() {
+  alert(Date(), tampil(), show());
+  Date();
+
+  let tanggal = new Date();
+  document.getElementById("hasil").innerHTML = tanggal;
+
+  function tampil() {
+    alert("website ini dibuat oleh Muhamad Fathur Irwan");
+  }
+  function show() {
+    alert(
+      "itu ada beberapa mobil sprot keren, semoga kita juga bisa memilikinya suatu saat nanti"
+    );
+  }
+}
